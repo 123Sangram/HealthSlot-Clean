5fd26390e7f50a1874cfbdbd15c1f68a122a92e8
123Sangram <150384931+123Sangram@users.noreply.github.com>
2025-10-21 19:14:15 +0530

    frontend url

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectCloudinary();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://healthslot-clean-frontend.onrender.com', 
   credentials: true, 
 }));
 
